53f148f828f290ff2074bbe02f050459920a945e
AMWolfstein <76236780+AMWolfstein@users.noreply.github.com>
2022-06-07 14:19:10 +0200

    sm6150-common: Build Pixel launcher mod by default

--- sm6150.mk
+++ sm6150.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Build Pixel Launcher Mod
+$(call inherit-product, vendor/PixelLauncher/PixelLauncher.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
